2cc861d7915cd867a64bd14c20a2b86387f873df
SaraKteifan <skteifan@gmail.com>
2025-04-22 12:30:16 +0300

    completed fractol

--- Practice/fract-ol/main.c
+++ Practice/fract-ol/main.c
@@ -3,10 +3,15 @@
 int	main(int ac, char **av)
 {
 	t_fractal	fractal;
-	
+
 	if (2 == ac && !ft_strncmp(av[1], "mandelbrot", 10) || 4 == ac && !ft_strncmp(av[1], "julia", 5))
 	{
-
+		fractal.name = av[1];
+		fractal.julia_x = av[2];
+		fractal.julia_y = av[3];
+		fractal_init(&fractal);
+		fractal_render(&fractal);
+		mlx_loop(fractal.mlx_connection);
 	}
 	else
 	{
